9c4ef0f9cbcb6c157f496992871055439414cd89
Bernard Limo <127617357+NeskoLimo@users.noreply.github.com>
2026-02-20 15:06:50 +0300

    Update projects.component.ts

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -2,6 +2,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 interface Mention {
   user: string;
@@ -29,7 +30,7 @@ interface Project {
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   template: `
     <div class="projects-page">
 
